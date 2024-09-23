7cef2de5bdb6ebf30bd850ef1a6c554b2c7451f9
Izhan Lara <izhanlaragarcia@gmail.com>
2024-09-23 14:00:55 +0200

    Inicio del proyecto

---
+++ src/components/UserSettings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserSettings() {
+  return (
+    <div className="settings-container">
+      <h1>Ajustes de Usuario</h1>
+      <p>Cambiar contraseña, notificaciones, etc.</p>
+    </div>
+  );
+}
+
+export default UserSettings;
