65d3697bc1f57ddcefb6cf06d07263d77a30006c
Izhan Lara García <izhanlaragarcia@gmail.com>
2024-09-23 14:00:28 +0200

    Delete OptiRuta directory

--- OptiRuta/src/api/auth.jsx
+++
@@ -1,9 +0,0 @@
-export const loginWithGoogle = () => {
-    return new Promise((resolve, reject) => {
-      // Simulación de login con Google, aquí iría la integración real con OAuth
-      setTimeout(() => {
-        resolve('Logged in');
-      }, 1000);
-    });
-  };
-  
